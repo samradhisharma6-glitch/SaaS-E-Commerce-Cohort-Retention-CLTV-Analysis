a15d453e5772d9803b983059d673aead170dbbd5
Samardhi Sharma <samradhisharma6@gmail.com>
2026-07-21 15:51:45 +0530

    week 3 work update

--- Sql/week 3  sql.sql
+++ Sql/week 3  sql.sql
@@ -223,3 +223,208 @@ SELECT * FROM RetentionMatrix
 
 
 -------------------------------------------------------------------
+--week 3 
+--Customer Revenue Summary
+SELECT
+    CustomerID,
+    SUM(Revenue) AS TotalRevenue
+INTO Customer_Revenue
+FROM online_retail_clean
+GROUP BY CustomerID;
+
+select* from Customer_Revenue
+
+----------------------------------------------------------------------
+--Total Orders per Customer
+
+SELECT
+    CustomerID,
+    COUNT(DISTINCT Invoice) AS TotalOrders
+INTO CustomerOrders
+FROM online_retail_clean
+GROUP BY CustomerID;
+
+select * from CustomerOrders
+
+---------------------------------------------------------------------------
+--Average Order Value (AOV)
+SELECT
+    r.CustomerID,
+    r.TotalRevenue,
+    o.TotalOrders,
+
+    ROUND(
+        r.TotalRevenue / o.TotalOrders,
+        2
+    ) AS AOV
+
+INTO CustomerAOV
+
+FROM CustomerRevenue r
+JOIN CustomerOrders o
+ON r.CustomerID = o.CustomerID;
+
+
+select * from CustomerAOV
+
+----------------------------------------------------------------------------------------
+
+--Purchase Frequency
+
+SELECT
+    COUNT(DISTINCT Invoice) * 1.0 /
+    COUNT(DISTINCT CustomerID)
+    AS PurchaseFrequency
+FROM online_retail_clean;
+
+-----------------------------------------------------------
+--Customer Value
+DECLARE @PurchaseFrequency FLOAT;
+
+SELECT
+    @PurchaseFrequency =
+    COUNT(DISTINCT Invoice) * 1.0 /
+    COUNT(DISTINCT CustomerID)
+FROM online_retail_clean;
+
+SELECT
+    CustomerID,
+    AOV,
+
+    ROUND(
+        AOV * @PurchaseFrequency,
+        2
+    ) AS CustomerValue
+
+INTO CustomerValue
+
+FROM CustomerAOV;
+
+select *from CustomerValue
+
+------------------------------------------------------------------------------
+--Historical CLTV
+SELECT
+    AVG(
+        DATEDIFF(
+            DAY,
+            MIN(InvoiceDate),
+            MAX(InvoiceDate)
+        )
+    )
+FROM online_retail_clean
+GROUP BY CustomerID;
+
+
+--
+WITH CustomerLifespans AS (
+    SELECT 
+        CustomerID,
+        DATEDIFF(
+            DAY,
+            MIN(InvoiceDate),
+            MAX(InvoiceDate)
+        ) AS LifespanDays
+    FROM online_retail_clean
+    GROUP BY CustomerID
+)
+SELECT AVG(CAST(LifespanDays AS FLOAT)) AS AvgLifespanDays
+FROM CustomerLifespans;
+
+---------------------------------------------------
+SELECT
+    CustomerID,
+    CustomerValue,
+
+    ROUND(
+        CustomerValue * 180,
+        2
+    ) AS CLTV
+
+INTO CustomerCLTV
+
+FROM CustomerValue;
+
+select * from
+CustomerCLTV
+
+
+---------------------------------------------------------------------------
+---Segment Customers
+
+SELECT
+    CustomerID,
+    CLTV,
+
+    CASE
+
+        WHEN CLTV >= 100000
+            THEN 'High Value'
+
+        WHEN CLTV >= 30000
+            THEN 'Medium Value'
+
+        ELSE 'Low Value'
+
+    END AS CustomerSegment
+
+INTO CustomerSegments
+
+FROM CustomerCLTV;
+
+select * from CustomerSegments
+
+-----------------------------------------------------------------------
+--Top Revenue Customers
+SELECT TOP 20
+
+    s.CustomerID,
+    r.TotalRevenue,
+    c.CLTV,
+    s.CustomerSegment
+
+FROM CustomerSegments s
+
+JOIN CustomerRevenue r
+ON s.CustomerID = r.CustomerID
+
+JOIN CustomerCLTV c
+ON s.CustomerID = c.CustomerID
+
+ORDER BY r.TotalRevenue DESC;
+
+-------------------------------------------------
+--Export CLTV Dataset
+SELECT
+    s.CustomerID,
+    r.TotalRevenue,
+    o.TotalOrders,
+    a.AOV,
+    c.CLTV,
+    s.CustomerSegment
+
+FROM CustomerSegments s
+
+JOIN CustomerRevenue r
+ON s.CustomerID = r.CustomerID
+
+JOIN CustomerOrders o
+ON s.CustomerID = o.CustomerID
+
+JOIN CustomerAOV a
+ON s.CustomerID = a.CustomerID
+
+JOIN CustomerCLTV c
+ON s.CustomerID = c.CustomerID;
+
+----------------------------------------------------------------------
+--Top Revenue-Contributing Customers
+SELECT TOP 10 *
+FROM online_retail_clean
+ORDER BY InvoiceDate DESC;
+
+
+SELECT TABLE_SCHEMA, TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_TYPE = 'BASE TABLE';
+
